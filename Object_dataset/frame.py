e50e1029509c8eb20d43a7378badd380b21b36b5
Yashasvi Trivedi <yashashvitrivedi6569224@gmail.com>
2022-11-11 23:58:49 +0530

    My First Commit

---
+++ Object_dataset/frame.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture('C:\\Users\Yashashvi\Documents\source_code\los_angeles.mp4')
+
+while True:
+
+ _, frame=cap.read()
+
+ cv2.imshow('Frame', frame)
+ cv2.waitKey(0)
